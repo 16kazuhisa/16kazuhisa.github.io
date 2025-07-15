0e175796cc55e0df0d84ab135c431b86742cac5d
16kazuhisa <naitoyuki0607@gmail.com>
2025-07-16 02:31:53 +0900

    20250716

--- forcast/awl.js
+++ forcast/awl.js
@@ -8,7 +8,6 @@ class MyWorkletProcessor extends AudioWorkletProcessor {
     let inputSamples  = inputs[0][0];
   
       this.port.postMessage(inputSamples);
-      console.log(inputSamples);
 
 return true;
 
